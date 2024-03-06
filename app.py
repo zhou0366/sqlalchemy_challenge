0036bafc08c0b82684ad5a965f2ae7834b902957
zhou0366 <zhou0366@umn.edu>
2024-03-05 22:05:48 -0600

    initial jupyter notebook

---
+++ app.py
@@ -0,0 +1,35 @@
+# Import the dependencies.
+import numpy as np
+from flask import Flask, jsonify
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
